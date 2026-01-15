bdef640741acaa0b1a24710000e7fffc96734f38
West <hu.wenjun1@olaola.chat>
2026-01-15 17:31:57 +0800

    优化部署脚本

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt update
+sudo apt install python3.12 python3.12-venv python3.12-dev -y
+sudo apt install -y git-lfs
 python3.12 -m venv .venv
 source .venv/bin/activate
 python -m pip install --upgrade pip
-git clone -b jp https://github.com/huwenjun1-ola/Akagi
+git clone -b jp https://github.com/huwenjun1-ola/Akagi2
 cp Akagi/mjai_bot/mortal/libriichi/libriichi-3.12-x86_64-unknown-linux-gnu.so Akagi/mjai_bot/mortal/libriichi.so
 python -m pip install -r Akagi/requirements.txt
