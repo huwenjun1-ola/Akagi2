4130c4a3631b7e56584093ac90f6d03dd6c7ceda
West <hu.wenjun1@olaola.chat>
2026-01-16 10:26:52 +0800

    优化配置读取

--- run_server.py
+++ run_server.py
@@ -1,10 +1,24 @@
 import os
 import sys
-
+import tomllib
 from entry import entry
 from loguru import logger as main_logger
 
+from settings.settings import settings
+
 if __name__ == '__main__':
+    try:
+        with open('../config/config.toml', 'rb') as f:
+            data = tomllib.load(f)
+            dNsq=data.get("go-nsq")
+            lAddrs=dNsq.get("game")
+            addr=lAddrs[0]
+            host,port=addr.split(":")
+            settings.mitm.host = host
+            settings.mitm.port = port
+            main_logger.debug(f"apply config from go,host:{host},port:{port}")
+    except Exception as e:
+        pass
     if os.environ.get("LOGURU_WRITEFILE", "1") == "0":
         main_logger.remove()
         # 2. 重新添加：仅绑定标准输出和标准错误
