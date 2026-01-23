519b878567961513e65d229c5b8f1052bbb0f0aa
West <hu.wenjun1@olaola.chat>
2026-01-23 14:25:30 +0800

    配置路径读取

--- run_server.py
+++ run_server.py
@@ -10,8 +10,8 @@ def safe_add_handler(*args, **kwargs):
     return None
 
 # 替换原始的 add 方法
+original_add = main_logger.add
 if os.environ.get("LOGURU_WRITEFILE", "1") == "0":
-    original_add = main_logger.add
     main_logger.add = safe_add_handler
 
 from entry import entry
@@ -24,13 +24,25 @@ if __name__ == '__main__':
             cfgPath=f"../{cfgPath}"
         with open(cfgPath, 'rb') as f:
             data = tomllib.load(f)
+            dLogger=data.get("logger")
+            levelStr=dLogger.get("Level")
+            main_logger.remove()
+            # 2. 重新添加：仅绑定标准输出和标准错误
+            # 通常建议：INFO 及以下去 stdout，WARNING 及以上去 stderr
+            if levelStr == "all":
+                original_add(sys.stdout, level="DEBUG")
+            else:
+                original_add(sys.stdout, level="INFO")
+            original_add(sys.stderr, level="WARNING")
             dNsq=data.get("go-nsq")
             lAddrs=dNsq.get("game")
-            addr=lAddrs[0]
-            host,port=addr.split(":")
-            settings.mitm.host = host
-            settings.mitm.port = port
-            main_logger.info(f"apply config from go,host:{host},port:{port}")
+            if lAddrs:
+                addr=lAddrs[0]
+                host,port=addr.split(":")
+                settings.mitm.host = host
+                settings.mitm.port = port
+                main_logger.info(f"apply config from go,host:{host},port:{port}")
+            main_logger.info(f"apply config from go")
     except Exception as e:
-        pass
+        main_logger.error(e)
     entry.main()
