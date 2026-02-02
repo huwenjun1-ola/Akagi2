a746932885f8c086e666b9f6c4acd80f7ba64fde
West <hu.wenjun1@olaola.chat>
2026-02-02 16:04:56 +0800

    安装说明更新

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ python3.12 -m venv .venv
 source .venv/bin/activate
 python -m pip install --upgrade pip
 git clone -b jp https://github.com/huwenjun1-ola/Akagi2
+cd Akagi && git lfs pull
 cp Akagi/mjai_bot/mortal/libriichi/libriichi-3.12-x86_64-unknown-linux-gnu.so Akagi/mjai_bot/mortal/libriichi.so
 python -m pip install -r Akagi/requirements.txt
