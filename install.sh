b64384b6646f63cb59b5882133f75d58b63c7a76
West <hu.wenjun1@olaola.chat>
2026-06-05 11:45:08 +0800

    支持3p

--- install.sh
+++ install.sh
@@ -8,4 +8,5 @@ python -m pip install --upgrade pip
 git clone -b jp https://github.com/huwenjun1-ola/Akagi2
 cd Akagi && git lfs pull
 cp Akagi/mjai_bot/mortal/libriichi/libriichi-3.12-x86_64-unknown-linux-gnu.so Akagi/mjai_bot/mortal/libriichi.so
+cp Akagi/mjai_bot/mortal3p/libriichi/libriichi3p-3.12-x86_64-unknown-linux-gnu.so Akagi/mjai_bot/mortal3p/libriichi3p.so
 python -m pip install -r Akagi/requirements.txt
